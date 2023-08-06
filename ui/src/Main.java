3b77d3da4b0b8aee4c70b49e824b79f7ccde1ee9
AwesomeSapir <melchinsapir@gmail.com>
2023-08-06 15:34:02 +0300

    Added an input validator

--- ui/src/Main.java
+++ ui/src/Main.java
@@ -1,5 +1,28 @@
+import validator.Validator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            System.out.println("is boolean: " + Validator
+                    .validate(input)
+                    .isBoolean()
+                    .isValid());
+            System.out.println("is name: " + Validator
+                    .validate(input)
+                    .isOnlyLetters()
+                    .isValid());
+            System.out.println("is double: " + Validator
+                    .validate(input)
+                    .isDouble()
+                    .isValid());
+            System.out.println("is integer: " + Validator
+                    .validate(input)
+                    .isInteger()
+                    .isValid());
+        }
     }
 }
---
+++ ui/src/validator/Validator.java
@@ -0,0 +1,63 @@
+package validator;
+
+public class Validator {
+
+    private boolean isValid = true;
+    private final String input;
+
+    private Validator(String input){
+        this.input = input;
+    }
+
+    public static Validator validate(String input){
+        return new Validator(input);
+    }
+
+    public boolean isValid(){
+        return isValid;
+    }
+
+    public Validator isInteger(){
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            isValid = false;
+        }
+        return this;
+    }
+
+    public Validator isDouble(){
+        try {
+            Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            isValid = false;
+        }
+        return this;
+    }
+
+    public Validator isInRange(double from, double to){
+        if (isDouble().isValid) {
+            double value = Double.parseDouble(input);
+            if(value > to || value < from){
+                isValid = false;
+            }
+        } else {
+            isValid = false;
+        }
+        return this;
+    }
+
+    public Validator isBoolean(){
+        if (!input.equalsIgnoreCase("true") && !input.equalsIgnoreCase("false")) {
+            isValid = false;
+        }
+        return this;
+    }
+
+    public Validator isOnlyLetters(){
+        if(!input.matches("^[a-zA-Z]+$")){
+            isValid = false;
+        }
+        return this;
+    }
+}
